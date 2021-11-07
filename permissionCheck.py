9a1a85dd05d253e67406ad5f10a423a83f1a9ba8
jeff29cc91079 <jeff29cc91079@gmail.com>
2021-11-07 22:23:29 +0800

    [update] basic permission check function(level 1 to 9)

---
+++ permissionCheck.py
@@ -0,0 +1,35 @@
+from os import getenv
+from function import getUserID
+import sqlite3
+
+def GetPermission(keyword):
+    if keyword in getenv("DEVELOPER_ID"):
+        return 9
+
+    sql = sqlite3.connect( 'Permission.db' )
+    cur = sql.cursor()
+    
+    cur.execute("Select level from Data where key = '{}'".format(keyword))
+
+    data = cur.fetchall()
+    
+    cur.close()
+    sql.close()
+
+    if len(data)==0:
+        return 1
+    else:
+        return data[0][0]
+
+
+def IsCommandAllowed(update, command = ''):
+    if command == '':
+        command = update.message.text
+    command = command.split(' ')[0].split('@')[0].split('/')[1]
+
+    userId = getUserID(update)
+
+    if GetPermission(userId)>=GetPermission(command):
+        return True
+    else:
+        return False
